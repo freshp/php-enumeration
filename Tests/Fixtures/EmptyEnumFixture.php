879ac33d1a7cf3be1ff7d577cb7d74f57a741859
freshp <freshprince.2b@gmx.net>
2019-11-29 14:43:07 +0100

    update php and quality tools

--- Tests/Fixtures/EmptyEnumFixture.php
+++ Tests/Fixtures/EmptyEnumFixture.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace FreshP\PhpEnumeration\Tests\Fixtures;
 
 use FreshP\PhpEnumeration\Enum;
--- Tests/Unit/EnumTest.php
+++ Tests/Unit/EnumTest.php
@@ -1,42 +1,46 @@
 <?php
 
+declare(strict_types=1);
+
 namespace FreshP\PhpEnumeration\Tests\Unit;
 
 use FreshP\PhpEnumeration\Tests\Fixtures\EmptyEnumFixture;
 use FreshP\PhpEnumeration\Tests\Fixtures\EnumFixture;
 use PHPUnit\Framework\TestCase;
+use stdClass;
+use TypeError;
 
 class EnumTest extends TestCase
 {
-    public function testDefaultEnumHandling()
+    public function testDefaultEnumHandling(): void
     {
         $enum = new EnumFixture();
 
         $this->assertEquals(EnumFixture::TEST_DEFAULT, $enum);
     }
 
-    public function testDefaultEnumHandlingWithConstructorValue()
+    public function testDefaultEnumHandlingWithConstructorValue(): void
     {
         $enum = new EnumFixture('definitely not set value');
 
         $this->assertEquals(EnumFixture::TEST_DEFAULT, $enum);
     }
 
-    public function testTypeErrorForUsingWrongDataType()
+    public function testTypeErrorForUsingWrongDataType(): void
     {
-        $this->expectException(\TypeError::class);
+        $this->expectException(TypeError::class);
 
-        new EnumFixture((new \stdClass));
+        new EnumFixture(new stdClass());
     }
 
-    public function testSuccessfulReturnValueForDefinedEnum()
+    public function testSuccessfulReturnValueForDefinedEnum(): void
     {
         $enum = new EnumFixture(EnumFixture::TEST_CONSTANT);
 
         $this->assertEquals(EnumFixture::TEST_CONSTANT, $enum);
     }
 
-    public function testReflectionException()
+    public function testReflectionException(): void
     {
         $enum = new EmptyEnumFixture();
 
